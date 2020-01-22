5a246929fe432ffb1499a6701d39ce28a019e234
gradonion <zhouyf@uchicago.edu>
2020-01-22 11:08:20 -0600

    manual

--- R/scalefaster.R
+++ R/scalefaster.R
@@ -1,3 +1,10 @@
+#' @title Fast CEntering and Scaling of Matrices
+#' @description Does the same thing as the default \code{scale(x)}, but faster.
+#' @details Uses \code{\link[matrixStats]{colSds}} from the 
+#'   \code{matrixStats} package.
+#' @param x A numeric matrix; \code{is.matrix(x)} should be \code{TRUE}
+#' @return A numeric matrix in which the columns are centered to have zero mean,
+#' and  they are also scaled to have standard deviation of 1.
 #' @export
 scale_faster <- function(x){
   a <- colMeans(x)
